9a5b75187d14640a1cbbb0d21d47683b4619d797
morrs <grishkovea@gmail.com>
2025-01-23 22:59:08 +0300

    feat: добавил библу для логера

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
