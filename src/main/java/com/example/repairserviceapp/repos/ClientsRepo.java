2a0837da13d0eba210959ed638e5a30c0839e72b
morrs <grishkovea@gmail.com>
2025-01-26 02:02:17 +0300

    feat: добавил репо для клиентов

---
+++ src/main/java/com/example/repairserviceapp/repos/ClientsRepo.java
@@ -0,0 +1,11 @@
+package com.example.repairserviceapp.repos;
+
+import com.example.repairserviceapp.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ClientsRepo extends JpaRepository<Client, UUID> {
+}
