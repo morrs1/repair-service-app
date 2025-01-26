6a514aa2b2e0d84c512af069f8c9c6546b0c47b6
morrs <grishkovea@gmail.com>
2025-01-26 21:37:46 +0300

    feat: добавил сущность для клиента

---
+++ src/main/java/com/example/repairserviceapp/entities/Client.java
@@ -0,0 +1,51 @@
+package com.example.repairserviceapp.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "clients")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Client {
+
+    @Id
+    @Column(name = "client_code")
+    private UUID id;
+
+    private String surname;
+
+    private String name;
+
+    private String patronymic;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @ToString.Exclude
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "client")
+    private List<Order> orders;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Client client = (Client) o;
+        return getId() != null && Objects.equals(getId(), client.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy proxy ? proxy.getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
