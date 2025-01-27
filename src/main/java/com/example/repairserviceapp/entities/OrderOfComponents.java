a85de0a4e52590db5cd26cb4b7c735a171c9ebfd
morrs <grishkovea@gmail.com>
2025-01-28 00:34:31 +0300

    feat: добавил связи для сущности заказа комплектующих

--- src/main/java/com/example/repairserviceapp/entities/OrderOfComponents.java
+++ src/main/java/com/example/repairserviceapp/entities/OrderOfComponents.java
@@ -1,11 +1,17 @@
 package com.example.repairserviceapp.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "components_order")
+@Setter
+@Getter
+@ToString
 public class OrderOfComponents {
 
     @Id
@@ -19,4 +25,7 @@ public class OrderOfComponents {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "execution_code", referencedColumnName = "ex_code")
     private ExecutionOfOrder executionOfOrder;
+
+    @OneToOne(mappedBy = "orderOfComponents")
+    private Order order;
 }
