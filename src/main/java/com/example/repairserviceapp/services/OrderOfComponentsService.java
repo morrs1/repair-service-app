3ccdc911538f48fca053c79275179deb30c6fbe6
morrs <grishkovea@gmail.com>
2025-02-02 00:54:56 +0300

    feat: добавил сервис для сущности заказа комплектующих

---
+++ src/main/java/com/example/repairserviceapp/services/OrderOfComponentsService.java
@@ -0,0 +1,52 @@
+package com.example.repairserviceapp.services;
+
+import com.example.repairserviceapp.entities.OrderOfComponents;
+import com.example.repairserviceapp.exceptions.EntityNotFoundException;
+import com.example.repairserviceapp.repos.OrderOfComponentsRepo;
+import jakarta.persistence.EntityManager;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class OrderOfComponentsService {
+
+    private final OrderOfComponentsRepo orderOfComponentsRepo;
+    private final EntityManager entityManager;
+
+    public List<OrderOfComponents> readAll() {
+        return orderOfComponentsRepo.findAll();
+    }
+
+    public OrderOfComponents read(UUID id) {
+        return orderOfComponentsRepo.findById(id).orElseThrow(() -> new EntityNotFoundException(id.toString()));
+    }
+
+    @Transactional
+    public OrderOfComponents create(OrderOfComponents orderOfComponents) {
+        orderOfComponents.setId(UUID.randomUUID());
+        return orderOfComponentsRepo.save(orderOfComponents);
+    }
+
+    @Transactional
+    public OrderOfComponents update(UUID id, OrderOfComponents orderOfComponents) {
+        orderOfComponentsRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("There is not order of components with this id"));
+        orderOfComponents.setId(id);
+        OrderOfComponents updatedOrderOfComponents = orderOfComponentsRepo.save(orderOfComponents);
+        entityManager.refresh(updatedOrderOfComponents);
+        return updatedOrderOfComponents;
+    }
+
+    @Transactional
+    public OrderOfComponents delete(UUID id) {
+        OrderOfComponents oldOrderOfComponents = orderOfComponentsRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("There is not order of components with this id"));
+        orderOfComponentsRepo.deleteById(id);
+        return oldOrderOfComponents;
+    }
+}
