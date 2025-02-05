05434192be65d40acdf20dea5b774333d3430031
morrs <grishkovea@gmail.com>
2025-02-05 15:41:47 +0300

    feat: понадобавлял JsonIgnore, потом удалить

--- src/main/java/com/example/repairserviceapp/entities/Order.java
+++ src/main/java/com/example/repairserviceapp/entities/Order.java
@@ -1,5 +1,6 @@
 package com.example.repairserviceapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,25 +25,32 @@ public class Order {
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="client_code", referencedColumnName = "client_code")
-    @ToString.Exclude
+
+    @JsonIgnore
     private Client client;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="equipment_code", referencedColumnName = "eq_code")
     @ToString.Exclude
+    @JsonIgnore
     private Equipment equipment;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="master_code")
     @ToString.Exclude
+    @JsonIgnore
     private Master master;
 
     @ManyToOne(fetch=FetchType.EAGER)
     @JoinColumn(name="order_status", referencedColumnName="status_code")
+    @ToString.Exclude
+    @JsonIgnore
     private Status status;
 
     @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="order_components_code", referencedColumnName = "co_code")
+    @JsonIgnore
+    @ToString.Exclude
     private OrderOfComponents orderOfComponents;
 
 
