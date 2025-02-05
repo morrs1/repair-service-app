32413b41da507a233fbd39330fe4df8b590328ad
morrs <grishkovea@gmail.com>
2025-02-05 15:41:17 +0300

    fix: исправил тип данных даты приема на работу

--- src/main/java/com/example/repairserviceapp/entities/Master.java
+++ src/main/java/com/example/repairserviceapp/entities/Master.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,7 +18,7 @@ public class Master {
 
     @Id
     @Column(name = "master_code")
-    private UUID master_code;
+    private UUID id;
 
     @Column(name = "surname")
     private String surname;
@@ -35,13 +36,13 @@ public class Master {
     private String phoneNumber;
 
     @Column(name = "date_of_employment")
-    private String dateOfEmployment;
+    private LocalDate dateOfEmployment;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "post_code", referencedColumnName = "post_code")
     private Post post;
 
-    @OneToMany(mappedBy="master")
+    @OneToMany(mappedBy = "master")
     @ToString.Exclude
     private List<Order> orders;
 }
