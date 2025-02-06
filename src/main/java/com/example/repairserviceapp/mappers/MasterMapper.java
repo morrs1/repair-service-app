875d1a464cd2fa48d2caf13c4f10a4e15268b606
morrs <grishkovea@gmail.com>
2025-02-07 00:36:49 +0300

    feat: добавил маппер для мастера

--- src/main/java/com/example/repairserviceapp/mappers/MasterMapper.java
+++ src/main/java/com/example/repairserviceapp/mappers/MasterMapper.java
@@ -1,8 +1,25 @@
 package com.example.repairserviceapp.mappers;
 
+import com.example.repairserviceapp.DTOs.MasterDTO;
+import com.example.repairserviceapp.entities.Master;
+import com.example.repairserviceapp.services.PostsService;
+import lombok.Setter;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper()
-public class MasterMapper {
 
+@Mapper(componentModel = "spring")
+
+public abstract class MasterMapper {
+
+    @Setter(onMethod = @__(@Autowired))
+    protected PostsService postsService;
+
+    @Mapping(target = "post", expression = "java(postsService.read(masterDTO.postId()))")
+    public abstract Master toMaster(MasterDTO masterDTO);
+
+    @Mapping(target = "postId", expression = "java(master.getPost().getId())")
+    public abstract MasterDTO toMasterDTO(Master master);
 }
+
