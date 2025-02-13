14a8a456d36f9aab9314d6cdb84ca51b09671b02
morrs <grishkovea@gmail.com>
2025-02-13 23:53:42 +0300

    fix: удалили entityManager из-за бага

--- src/main/java/com/example/repairserviceapp/services/MastersService.java
+++ src/main/java/com/example/repairserviceapp/services/MastersService.java
@@ -3,7 +3,6 @@ package com.example.repairserviceapp.services;
 import com.example.repairserviceapp.entities.Master;
 import com.example.repairserviceapp.exceptions.EntityNotFoundException;
 import com.example.repairserviceapp.repos.MastersRepo;
-import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,13 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.UUID;
 
+
 @Service
 @Transactional(readOnly = true)
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class MastersService {
 
     private final MastersRepo mastersRepo;
-    private final EntityManager entityManager;
 
     public List<Master> readAll() {
         return mastersRepo.findAll();
@@ -38,9 +37,7 @@ public class MastersService {
     public Master update(UUID id, Master master) {
         mastersRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("There is no master with this id"));
         master.setId(id);
-        Master updatedMaster = mastersRepo.save(master);
-        entityManager.refresh(updatedMaster);
-        return updatedMaster;
+        return mastersRepo.save(master);
     }
 
     @Transactional
