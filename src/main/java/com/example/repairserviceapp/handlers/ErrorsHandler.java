fa59158ff42d744394bb86c744816f1d837e8f9a
morrs <grishkovea@gmail.com>
2025-02-14 23:23:46 +0300

    feat: добаил хэндлер для ошибки валидации

--- src/main/java/com/example/repairserviceapp/handlers/ErrorsHandler.java
+++ src/main/java/com/example/repairserviceapp/handlers/ErrorsHandler.java
@@ -1,6 +1,7 @@
 package com.example.repairserviceapp.handlers;
 
 import com.example.repairserviceapp.exceptions.ApplicationException;
+import com.example.repairserviceapp.exceptions.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,7 +15,19 @@ public class ErrorsHandler {
 
     @ExceptionHandler(ApplicationException.class)
     public ResponseEntity<ErrorResponse> exceptionHandler(ApplicationException ex) {
-        log.warn("{} {}",ex.getClass().getSimpleName(), ex.getMessage());
-        return ResponseEntity.status(ex.getStatus()).body(new ErrorResponse(ex.getMessage(), LocalDateTime.now()));
+        log.warn("{} {}", ex.getClass().getSimpleName(), ex.getMessage());
+        return ResponseEntity.status(ex.getStatus()).body(new ErrorResponse(ex.getMessage(), LocalDateTime.now(), ex.getStatus()));
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<ValidationErrorResponse> validationExceptionHandler(ValidationException ex) {
+        log.warn("{} {}", ex.getClass().getSimpleName(), ex.getMessage());
+        return ResponseEntity.status(ex.getStatus())
+                .body(
+                        new ValidationErrorResponse(ex.getMessage(),
+                                LocalDateTime.now(),
+                                ex.getDescriptionOfErrors(),
+                                ex.getStatus())
+                );
     }
 }
