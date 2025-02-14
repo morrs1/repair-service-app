bbb01d2344842a29f15e90e0e12429a7a31974f1
morrs <grishkovea@gmail.com>
2025-02-14 23:23:08 +0300

    fix: попривал сообщения для ошибки валидации

--- src/main/java/com/example/repairserviceapp/DTOs/MasterDTO.java
+++ src/main/java/com/example/repairserviceapp/DTOs/MasterDTO.java
@@ -12,19 +12,19 @@ public record MasterDTO(
         UUID id,
 
         @NotNull(message = "Surname must be not empty")
-        @Size(min = 2, max = 30)
+        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
         String surname,
 
         @NotNull(message = "Name must be not empty")
-        @Size(min = 2, max = 30)
+        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
         String name,
 
         @NotNull(message = "Patronymic must be not empty")
-        @Size(min = 2, max = 30)
+        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
         String patronymic,
 
         @NotNull(message = "Address must be not empty")
-        @Size(min = 2, max = 30)
+        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
         String address,
 
         @NotNull(message = "Phone number must be not empty")
