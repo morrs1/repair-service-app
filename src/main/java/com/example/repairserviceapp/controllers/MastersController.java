ac30ce60027e88ee4fa3132b9dff495a33d5aeda
morrs <grishkovea@gmail.com>
2025-02-14 14:49:53 +0300

    feat: готовая ручка, кроме валидации

--- src/main/java/com/example/repairserviceapp/controllers/MastersController.java
+++ src/main/java/com/example/repairserviceapp/controllers/MastersController.java
@@ -2,49 +2,59 @@ package com.example.repairserviceapp.controllers;
 
 import com.example.repairserviceapp.DTOs.MasterDTO;
 import com.example.repairserviceapp.entities.Master;
+import com.example.repairserviceapp.exceptions.MastersValidationException;
 import com.example.repairserviceapp.mappers.MasterMapper;
 import com.example.repairserviceapp.services.MastersService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/master")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class MastersController {
 
-    //TODO добавить валидацию на дто и валидацию из бд
-
     private final MastersService mastersService;
     private final MasterMapper masterMapper;
 
     @GetMapping("")
-    public List<Master> readAll() {
-        return mastersService.readAll();
+    public List<MasterDTO> readAll() {
+        return mastersService.readAll().stream().map(masterMapper::toMasterDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public Master read(@PathVariable UUID id) {
-        return mastersService.read(id);
+    public MasterDTO read(@PathVariable UUID id) {
+        return masterMapper.toMasterDTO(mastersService.read(id));
     }
 
     @PostMapping("")
-    public MasterDTO create(@RequestBody MasterDTO masterDTO) {
+    public MasterDTO create(@RequestBody @Valid MasterDTO masterDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new MastersValidationException("Validation Error: " + bindingResult.getAllErrors().stream().map(
+                    DefaultMessageSourceResolvable::getDefaultMessage).toList()
+            );
+
+        }
         Master master = masterMapper.toMaster(masterDTO);
-        System.out.println(master);
         return masterMapper.toMasterDTO(mastersService.create(master));
     }
 
     @PatchMapping("/{id}")
-    public Master update(@PathVariable UUID id, @RequestBody Master master) {
-        return mastersService.update(id, master);
+    public MasterDTO update(@PathVariable UUID id, @RequestBody MasterDTO masterDTO) {
+        return masterMapper.toMasterDTO(mastersService.update(id, masterMapper.toMaster(masterDTO)));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable UUID id) {
-        mastersService.delete(id);
+    public MasterDTO delete(@PathVariable UUID id) {
+        return masterMapper.toMasterDTO(mastersService.delete(id));
     }
 }
