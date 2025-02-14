96521e995a58d58af280668892364d4c56b7ab67
morrs <grishkovea@gmail.com>
2025-02-14 14:49:21 +0300

    fix: исправил описание ошибкой валидации

--- src/main/java/com/example/repairserviceapp/DTOs/MasterDTO.java
+++ src/main/java/com/example/repairserviceapp/DTOs/MasterDTO.java
@@ -8,26 +8,33 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 public record MasterDTO(
-        @NotNull(message = "id must be not empty")
+        @NotNull(message = "Id must be not empty")
         UUID id,
-        @NotNull(message = "id must be not empty")
+
+        @NotNull(message = "Surname must be not empty")
         @Size(min = 2, max = 30)
         String surname,
-        @NotNull(message = "id must be not empty")
+
+        @NotNull(message = "Name must be not empty")
         @Size(min = 2, max = 30)
         String name,
-        @NotNull(message = "id must be not empty")
+
+        @NotNull(message = "Patronymic must be not empty")
         @Size(min = 2, max = 30)
         String patronymic,
-        @NotNull(message = "id must be not empty")
+
+        @NotNull(message = "Address must be not empty")
         @Size(min = 2, max = 30)
         String address,
-        @NotNull(message = "id must be not empty")
+
+        @NotNull(message = "Phone number must be not empty")
         @Pattern(regexp = "^(\\+7|8)[0-9]{10}$", message = "Phone number must be valid")
         String phoneNumber,
-        @NotNull(message = "id must be not empty")
+
+        @NotNull(message = "Date of employment must be not empty")
         LocalDate dateOfEmployment,
-        @NotNull(message = "id must be not empty")
+
+        @NotNull(message = "post id must be not empty")
         UUID postId
 ) {
 }
