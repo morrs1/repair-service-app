366729f9a459e1b9e9f4dd27a4c9bc607f502f95
morrs <grishkovea@gmail.com>
2025-02-16 23:01:09 +0300

    fix: удалил анноатации для toString и JSON

--- src/main/java/com/example/repairserviceapp/entities/Order.java
+++ src/main/java/com/example/repairserviceapp/entities/Order.java
@@ -1,6 +1,5 @@
 package com.example.repairserviceapp.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,35 +24,22 @@ public class Order {
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="client_code", referencedColumnName = "client_code")
-
-    @JsonIgnore
     private Client client;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="equipment_code", referencedColumnName = "eq_code")
-    @ToString.Exclude
-    @JsonIgnore
     private Equipment equipment;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="master_code")
-    @ToString.Exclude
-    @JsonIgnore
     private Master master;
 
     @ManyToOne(fetch=FetchType.EAGER)
     @JoinColumn(name="order_status", referencedColumnName="status_code")
-    @ToString.Exclude
-    @JsonIgnore
     private Status status;
 
     @OneToOne(fetch = FetchType.EAGER)
     @JoinColumn(name="order_components_code", referencedColumnName = "co_code")
-    @JsonIgnore
-    @ToString.Exclude
     private OrderOfComponents orderOfComponents;
-
-
-
 
 }
