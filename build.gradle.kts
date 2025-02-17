7f3fd7f8718aa883e841e329f95bde8f4ce3f6b6
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-17 17:14:55 +0300

    added swagger to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("net.logstash.logback:logstash-logback-encoder:8.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.4")
 
     compileOnly( "org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
--- src/main/java/com/example/repairserviceapp/RepairServiceAppApplication.java
+++ src/main/java/com/example/repairserviceapp/RepairServiceAppApplication.java
@@ -1,8 +1,19 @@
 package com.example.repairserviceapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Repair Service App",
+                description = "API системы компьютерной мастерской",
+                version = "1.0.0",
+                contact = @Contact(name = "Grishkov Egor")
+        )
+)
 @SpringBootApplication
 public class RepairServiceAppApplication {
 
