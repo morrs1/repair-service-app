1b12312f7036bc8e9d905a44826e6b34e9e6eb74
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-17 17:15:28 +0300

    added example of documentation to endpoint of clients

--- src/main/java/com/example/repairserviceapp/controllers/ClientsController.java
+++ src/main/java/com/example/repairserviceapp/controllers/ClientsController.java
@@ -3,6 +3,8 @@ package com.example.repairserviceapp.controllers;
 import com.example.repairserviceapp.DTOs.ClientDTO;
 import com.example.repairserviceapp.mappers.ClientsMapper;
 import com.example.repairserviceapp.services.ClientsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+@Tag(name = "Контроллер для управления клиентами", description = "Здесь есть все операции CRUD для клиента")
 @RestController
 @RequestMapping("/api/client")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -21,30 +24,52 @@ public class ClientsController extends BaseController {
     private ClientsService clientsService;
     private ClientsMapper clientsMapper;
 
+    @Operation(
+            summary = "Просмотр данных всех пользователей",
+            description = "Позволяет просмотреть всех пользователей."
+    )
     @GetMapping("")
     public List<ClientDTO> readAll() {
-        return clientsService.readAll().stream().map(
-                client -> clientsMapper.toDTO(client)
-        ).collect(Collectors.toList());
+        return clientsService
+                .readAll()
+                .stream()
+                .map(client -> clientsMapper.toDTO(client))
+                .collect(Collectors.toList());
     }
 
+    @Operation(
+            summary = "Просмотр данных об одном пользователя",
+            description = "Позволяет посмотреть данные об одном пользователе, зная его уникальный идентификатор. "
+    )
     @GetMapping("/{id}")
     public ClientDTO read(@PathVariable("id") UUID id) {
         return clientsMapper.toDTO(clientsService.read(id));
     }
 
+    @Operation(
+            summary = "Создание одного пользователя",
+            description = "Позволяет создать одного отдельного пользователя"
+    )
     @PostMapping("")
     public ClientDTO create(@RequestBody @Valid ClientDTO clientDTO, BindingResult bindingResult) {
         validate(bindingResult, "Create client failed");
         return clientsMapper.toDTO(clientsService.create(clientsMapper.toClient(clientDTO)));
     }
 
+    @Operation(
+            summary = "Обновление данных отдельного пользователя",
+            description = "Позволяет обновлять данные об отдельном пользователе, зная его id. "
+    )
     @PatchMapping("/{id}")
     public ClientDTO update(@PathVariable("id") UUID id, @RequestBody @Valid ClientDTO clientDTO, BindingResult bindingResult) {
         validate(bindingResult, "Update client failed");
         return clientsMapper.toDTO(clientsService.update(id, clientsMapper.toClient(clientDTO)));
     }
 
+    @Operation(
+            summary = "Удаление данных об отдельном пользователе",
+            description = "Позволяет удалять данные об отдельном пользователе, зная его id"
+    )
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id") UUID id) {
         clientsService.delete(id);
