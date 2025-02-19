ead4d557393e7a108b447843bd085668f9a2b2d9
morrs <grishkovea@gmail.com>
2025-02-19 22:40:55 +0300

    fix: поменял типы возвращаемые типы данных

--- src/main/java/com/example/repairserviceapp/controllers/MastersController.java
+++ src/main/java/com/example/repairserviceapp/controllers/MastersController.java
@@ -1,8 +1,8 @@
 package com.example.repairserviceapp.controllers;
 
-import com.example.repairserviceapp.DTOs.MasterDTO;
+import com.example.repairserviceapp.DTOs.master.MasterDTORequest;
+import com.example.repairserviceapp.DTOs.master.MasterDTOResponse;
 import com.example.repairserviceapp.entities.Master;
-import com.example.repairserviceapp.exceptions.ValidationException;
 import com.example.repairserviceapp.mappers.MasterMapper;
 import com.example.repairserviceapp.services.MastersService;
 import jakarta.validation.Valid;
@@ -11,9 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -27,30 +25,30 @@ public class MastersController extends BaseController {
     private final MasterMapper masterMapper;
 
     @GetMapping("")
-    public List<MasterDTO> readAll() {
+    public List<MasterDTOResponse> readAll() {
         return mastersService.readAll().stream().map(masterMapper::toMasterDTO).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public MasterDTO read(@PathVariable UUID id) {
+    public MasterDTOResponse read(@PathVariable UUID id) {
         return masterMapper.toMasterDTO(mastersService.read(id));
     }
 
     @PostMapping("")
-    public MasterDTO create(@RequestBody @Valid MasterDTO masterDTO, BindingResult bindingResult) {
+    public MasterDTOResponse create(@RequestBody @Valid MasterDTORequest masterDTORequest, BindingResult bindingResult) {
         validate(bindingResult, "Create master failed");
-        Master master = masterMapper.toMaster(masterDTO);
+        Master master = masterMapper.toMaster(masterDTORequest);
         return masterMapper.toMasterDTO(mastersService.create(master));
     }
 
     @PatchMapping("/{id}")
-    public MasterDTO update(@PathVariable UUID id, @RequestBody @Valid MasterDTO masterDTO, BindingResult bindingResult) {
+    public MasterDTOResponse update(@PathVariable UUID id, @RequestBody @Valid MasterDTORequest masterDTORequest, BindingResult bindingResult) {
         validate(bindingResult, "Update master failed");
-        return masterMapper.toMasterDTO(mastersService.update(id, masterMapper.toMaster(masterDTO)));
+        return masterMapper.toMasterDTO(mastersService.update(id, masterMapper.toMaster(masterDTORequest)));
     }
 
     @DeleteMapping("/{id}")
-    public MasterDTO delete(@PathVariable UUID id) {
+    public MasterDTOResponse delete(@PathVariable UUID id) {
         return masterMapper.toMasterDTO(mastersService.delete(id));
     }
 
