99d889c8b267aa2c4023b6812e6a921d28ef8241
morrs <grishkovea@gmail.com>
2025-02-19 23:36:07 +0300

    feat: добавил разделение дто заказа компонентов на ответ и запрос

--- src/main/java/com/example/repairserviceapp/DTOs/orderOfComponents/OrderOfComponentsDTORequest.java
+++ src/main/java/com/example/repairserviceapp/DTOs/orderOfComponents/OrderOfComponentsDTORequest.java
@@ -1,11 +1,10 @@
-package com.example.repairserviceapp.DTOs;
+package com.example.repairserviceapp.DTOs.orderOfComponents;
 
 import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
-public record OrderOfComponentsDTO(
-        UUID id,
+public record OrderOfComponentsDTORequest(
         @NotNull(message = "Components of warehouse id must be not empty")
         UUID componentsWarehouseId,
         @NotNull(message = "Execution of order id must be not empty")
