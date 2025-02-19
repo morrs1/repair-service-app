d6ca7d29af4ff5d60be64c2a406f6d65c81b8ec4
morrs <grishkovea@gmail.com>
2025-02-19 15:09:43 +0300

    feat: добавил маппер для заказа компонентов

---
+++ src/main/java/com/example/repairserviceapp/mappers/OrderOfComponentsMapper.java
@@ -0,0 +1,30 @@
+package com.example.repairserviceapp.mappers;
+
+import com.example.repairserviceapp.DTOs.OrderOfComponentsDTO;
+import com.example.repairserviceapp.entities.OrderOfComponents;
+import com.example.repairserviceapp.services.ComponentsWarehouseService;
+import com.example.repairserviceapp.services.ExecutionOfOrderService;
+import com.example.repairserviceapp.services.OrdersService;
+import lombok.Setter;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Mapper(componentModel = "spring")
+public abstract class OrderOfComponentsMapper {
+
+    @Setter(onMethod = @__(@Autowired))
+    protected ComponentsWarehouseService componentsWarehouseService;
+
+    @Setter(onMethod = @__(@Autowired))
+    protected ExecutionOfOrderService executionOfOrderService;
+
+
+    @Mapping(target = "componentsWarehouse", expression = "java(componentsWarehouseService.read(orderOfComponentsDTO.componentsWarehouseId()))")
+    @Mapping(target = "executionOfOrder", expression = "java(executionOfOrderService.read(orderOfComponentsDTO.executionOfOrderId()))")
+    public abstract OrderOfComponents toOrderOfComponents(OrderOfComponentsDTO orderOfComponentsDTO);
+
+    @Mapping(target = "componentsWarehouseId", expression = "java(orderOfComponents.getComponentsWarehouse().getId())")
+    @Mapping(target = "executionOfOrderId", expression = "java(orderOfComponents.getExecutionOfOrder().getId())")
+    public abstract OrderOfComponentsDTO toDTO(OrderOfComponents orderOfComponents);
+}
