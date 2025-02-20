b58fe64ce71cd6138bf1f27c6b3bffbf8cb80bbd
morrs <grishkovea@gmail.com>
2025-02-21 02:02:18 +0300

    feat: доделать конфиг

--- src/main/java/com/example/repairserviceapp/configs/SecurityConfiguration.java
+++ src/main/java/com/example/repairserviceapp/configs/SecurityConfiguration.java
@@ -1,21 +1,21 @@
 package com.example.repairserviceapp.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    //TODO добавить в дто клиента email
+
+
 //    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws SecurityException {
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 //        return httpSecurity.authorizeHttpRequests(registry -> {
-//            registry.anyRequest().authenticated();
-//            registry.anyRequest().permitAll();
-//        })
+//            registry.requestMatchers("/api/history/**").hasRole("ADMIN");
+////            registry.anyRequest().authenticated();
+//        }).build();
 //    }
 
 }
