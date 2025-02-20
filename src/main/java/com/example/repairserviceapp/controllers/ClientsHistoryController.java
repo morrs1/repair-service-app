ae56714c288d62770a335de0fa35665e05e2125d
morrs <grishkovea@gmail.com>
2025-02-20 23:25:17 +0300

    fix: поменял url для ручки клиентов

--- src/main/java/com/example/repairserviceapp/controllers/ClientsHistoryController.java
+++ src/main/java/com/example/repairserviceapp/controllers/ClientsHistoryController.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 
 @Tag(name = "Контроллер для управления историей клиентов", description = "Здесь реализуется свойство темпоральности")
 @RestController
-@RequestMapping("/api/client/history")
+@RequestMapping("/api/history/client")
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class ClientsHistoryController extends BaseController {
     private ClientsService clientsService;
