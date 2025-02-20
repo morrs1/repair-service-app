cb8a71fd9ce6a50c4cba27c624768b63824856d3
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-20 22:56:54 +0300

    new jpa repo!

--- src/main/java/com/example/repairserviceapp/repos/PostsHistoryRepo.java
+++ src/main/java/com/example/repairserviceapp/repos/PostsHistoryRepo.java
@@ -1,4 +1,20 @@
 package com.example.repairserviceapp.repos;
 
-public interface PostsHistoryRepo {
+import com.example.repairserviceapp.entities.PostHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.OffsetDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PostsHistoryRepo extends JpaRepository<PostHistory, UUID> {
+    @Query(
+            value = "SELECT * FROM posts_history WHERE post_code = :id AND sys_period @> (:timestamp)::TIMESTAMPTZ",
+            nativeQuery = true
+    )
+    Optional<PostHistory> findByPostIdAndTimestamp(@Param("id") UUID id,
+                                                   @Param("timestamp") OffsetDateTime timestamp
+    );
 }
