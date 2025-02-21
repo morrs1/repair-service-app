d4860fe1c39c8bdc650a78db16f91e01d1c54231
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-21 15:50:09 +0300

    added method in mapper and dto response history for components warehouse

---
+++ src/main/java/com/example/repairserviceapp/DTOs/componentsWarehouse/ComponentsWarehouseHistoryDTOResponse.java
@@ -0,0 +1,24 @@
+package com.example.repairserviceapp.DTOs.componentsWarehouse;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public record ComponentsWarehouseHistoryDTOResponse(
+        UUID id,
+        @NotNull(message = "Components name must be not null")
+        @NotEmpty(message = "Components name must be not empty")
+        String componentName,
+        @NotNull(message = "Components amount must be not null")
+        @Min(value = 0, message = "Components amount must be greater than 0")
+        Integer componentsAmount,
+        @NotNull(message = "Components cost must be not null")
+        BigDecimal unitCost,
+        @NotNull(message = "Zoned date time must be not empty")
+        OffsetDateTime offsetDateTime
+) {
+}
