46229022d803948b0c298b0e0830b564e85a90ad
morrs <grishkovea@gmail.com>
2025-02-22 21:11:02 +0300

    feat: добавил контроллер для аутентификации

---
+++ src/main/java/com/example/repairserviceapp/controllers/AuthController.java
@@ -0,0 +1,38 @@
+package com.example.repairserviceapp.controllers;
+
+import com.example.repairserviceapp.webToken.JwtService;
+import com.example.repairserviceapp.webToken.LoginForm;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor(onConstructor = @__(@Autowired))
+public class AuthController {
+
+    private AuthenticationManager authenticationManager;
+
+    private JwtService jwtService;
+
+    private UserDetailsService userDetailsService;
+
+    @PostMapping("/api/auth")
+    public String authenticateAndGetToken(@RequestBody LoginForm loginForm) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginForm.username(), loginForm.password()
+        ));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(userDetailsService.loadUserByUsername(loginForm.username()));
+        } else {
+            throw new UsernameNotFoundException("Invalid credentials");
+        }
+    }
+
+}
