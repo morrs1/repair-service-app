eec967c0955a2904e0ab1dbdfb192fbcec7635e1
morrs <grishkovea@gmail.com>
2025-02-22 21:10:39 +0300

    feat: добавил метод в тесты для генерации секрета для JWT

--- src/test/java/com/example/repairserviceapp/RepairServiceAppApplicationTests.java
+++ src/test/java/com/example/repairserviceapp/RepairServiceAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.repairserviceapp;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.xml.bind.DatatypeConverter;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,11 @@ class RepairServiceAppApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void generateSecretKey() {
+        System.out.println(DatatypeConverter.printHexBinary(Jwts.SIG.HS512.key().build().getEncoded()));
     }
 
 }
