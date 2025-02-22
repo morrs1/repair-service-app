56e1d3408c795f74308bb9090c8379080ba68afe
morrs <grishkovea@gmail.com>
2025-02-22 21:09:37 +0300

    feat: добавил зависимости для работы с JWT

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,10 @@ dependencies {
     implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.9.2")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
 
     compileOnly( "org.mapstruct:mapstruct:1.6.3")
     annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
