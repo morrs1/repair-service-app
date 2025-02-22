ada5eb2eff93c7c75bfea885ff6704d60e72c870
morrs <grishkovea@gmail.com>
2025-02-23 00:21:26 +0300

    feat: переделал дто на два вида

--- src/main/java/com/example/repairserviceapp/DTOs/client/ClientDTOResponse.java
+++ src/main/java/com/example/repairserviceapp/DTOs/client/ClientDTOResponse.java
@@ -1,6 +1,7 @@
-package com.example.repairserviceapp.DTOs;
+package com.example.repairserviceapp.DTOs.client;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -8,7 +9,7 @@ import jakarta.validation.constraints.Size;
 import java.util.UUID;
 
 @Schema(description = "User entity that uses our product")
-public record ClientDTO(
+public record ClientDTOResponse(
 
         @Schema(
                 description = "Unique identifier of client (uuid)",
@@ -19,22 +20,28 @@ public record ClientDTO(
 
         @Schema(description = "Surname of client", example = "Ivanov")
         @NotNull(message = "Surname must be not empty")
-        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
+        @Size(min = 2, max = 30, message = "Size of surname must be between 2 and 30")
         String surname,
 
         @Schema(description = "Name of client", example = "Ivan")
         @NotNull(message = "Name must be not empty")
-        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
+        @Size(min = 2, max = 30, message = "Size of name must be between 2 and 30")
         String name,
 
         @Schema(description = "Patronymic of client", example = "Ivanovich")
         @NotNull(message = "Patronymic must be not empty")
-        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
+        @Size(min = 2, max = 30, message = "Size of patronymic must be between 2 and 30")
         String patronymic,
 
         @Schema(description = "Phone number of client", example = "+71812554589")
         @NotNull(message = "Phone number must be not empty")
         @Pattern(regexp = "^(\\+7|8)[0-9]{10}$", message = "Phone number must be valid")
-        String phoneNumber
+        String phoneNumber,
+
+        @NotNull(message = "Email must be not null")
+        @Email(message = "Email must be valid")
+        @Size(min = 2, max = 30, message = "Size must be between 2 and 30")
+        String email
+
 ) {
 }
