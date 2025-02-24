3e05d77d3c63343b2f37094473a3047ad668a4cc
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-24 19:40:34 +0300

    changed entities because inheritance was making duplicates

--- src/main/java/com/example/repairserviceapp/entities/MasterHistory.java
+++ src/main/java/com/example/repairserviceapp/entities/MasterHistory.java
@@ -1,9 +1,25 @@
 package com.example.repairserviceapp.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
 
 @Entity
 @Table(name = "masters_list_history")
-public class MasterHistory extends Master {
+@Getter
+@Setter
+@ToString
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MasterHistory extends BaseMaster {
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "post_code", referencedColumnName = "post_code")
+    private PostHistory post;
+
+    @OneToMany(mappedBy = "master")
+    @ToString.Exclude
+    private List<OrderHistory> orders;
 }
